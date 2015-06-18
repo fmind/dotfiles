a502b1426dd4d8be34c5aaa99fc861aa61856ac3
Freaxmind <freaxmind@users.noreply.github.com>
2015-06-18 19:33:07 +0200

    Update tool settings: vim, byobu, xfce, git, bash, ipython

---
+++ ipython/profile_freaxmind/ipython_config.py
@@ -0,0 +1,18 @@
+# Configuration file for ipython.
+
+c = get_config()
+
+c.InteractiveShellApp.exec_lines = [
+    'import datetime',
+    'import time',
+    'import math',
+    'import sys',
+    'import os',
+    'import re',
+]
+
+c.TerminalInteractiveShell.editor = "vim"
+c.TerminalInteractiveShell.colors="Linux"
+c.TerminalIPythonApp.display_banner = False
+c.TerminalInteractiveShell.confirm_exit=False
+c.PromptManager.in_template = "{color.DarkGray}\T>{color.Green}fre@x{color.Cyan}~In[{color.Yellow}\N{color.Cyan}]{color.normal}: "
