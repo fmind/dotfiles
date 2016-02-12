f48b8164c582fe2385fffb9039495fc88a7d7ae4
Freaxmind <freaxmind@users.noreply.github.com>
2016-02-12 12:56:26 +0100

    Fixed bug with lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,4 @@
 {:user {:plugins [[lein-exec "0.3.6"]
-                  [cider/cider-nrepl "0.10.2"]]}}
+                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
