da2d1f2697a10d5cd5703dd852b2a1f69f11f7bd
Freaxmind <freaxmind@users.noreply.github.com>
2016-02-12 10:04:01 +0100

    Removed warning caused by byobu

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,2 @@
-{:user {:plugins [[lein-exec "0.3.6"]]}}
-
-{:repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
-        :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}}
+{:user {:plugins [[lein-exec "0.3.6"]
+                  [cider/cider-nrepl "0.10.2"]]}}
