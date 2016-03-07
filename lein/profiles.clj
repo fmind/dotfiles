ccd1d29c2d51f0339b4cf55e40ecc2a09d0228bf
Freaxmind <freaxmind@users.noreply.github.com>
2016-03-07 15:38:24 +0100

    Updated lein config + minor changes

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,9 @@
-{:user {:plugins [[lein-exec "0.3.6"]
-                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+{
+ :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]        
+        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]}
+ :user {:plugins [[lein-exec "0.3.6"]
+                  [lein-ancient "0.6.8"]]
+        :dependencies [[slamhound "1.5.5"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
+}
