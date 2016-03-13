5f726a60283eacfe5b1be735a87acc0ad965083e
Freaxmind <freaxmind@users.noreply.github.com>
2016-03-13 22:29:42 +0100

    Modified lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,7 +3,13 @@
         :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]}
  :user {:plugins [[lein-exec "0.3.6"]
-                  [lein-ancient "0.6.8"]]
+                  [lein-ancient "0.6.8"]
+                  [lein-cljfmt "0.4.1"]
+                  [lein-cloverage "1.0.6"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-kibit "0.1.2"]
+                  [lein-bikeshed "0.3.0"]
+                  [venantius/ultra "0.4.1"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
 }
