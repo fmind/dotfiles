d9b1a2be1216e8d74be545974a40b5fcdc9ce4ad
Freaxmind <freaxmind@users.noreply.github.com>
2016-04-15 15:09:50 +0200

    Updated profiles.clj + new snippets

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,12 @@
 {
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]        
-        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]}
+        :plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
+                  [refactor-nrepl "2.3.0-SNAPSHOT"]]}
 
  :user {:plugins [[lein-exec "0.3.6"]
                   [lein-kibit "0.1.2"]
                   [lein-cljfmt "0.4.1"]
+                  [lein-gorilla "0.3.6"]
                   [lein-ancient "0.6.8"]
                   [lein-bikeshed "0.3.0"]
                   [lein-cloverage "1.0.6"]
