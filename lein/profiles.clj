ef1006a2c6fcf7d11651dccdc4ab5b4f4a9f2ecd
Freaxmind <freaxmind@users.noreply.github.com>
2016-05-12 12:17:31 +0200

    Added more clj profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -20,15 +20,37 @@
                       clojure.core > [clojure.pprint pprint] [clojure.java.shell sh])]
         }
 
+ :http [:user
+        {:dependencies [[http-kit "2.1.19"]
+                        [enlive "1.1.6"]]
+         :injections [(require '[org.httpkit.client :as http])
+                      (use 'net.cgrand.enlive-html)
+                      (import java.net.URL)]}]
+
  :selenium [:user
             {:dependencies [[clj-webdriver/clj-webdriver "0.7.2"]
                            [org.seleniumhq.selenium/selenium-java "2.52.0"]]
             :injections [(use 'clj-webdriver.taxi)
                          (set-driver! {:browser :firefox})]}]
-
  :maths [:user
          {:dependencies [[org.clojure/math.combinatorics "0.1.1"]
                          [org.clojure/math.numeric-tower "0.0.4"]]
           :injections [(use 'clojure.math.combinatorics)
                        (use 'clojure.math.numeric-tower)]}]
+
+ :stats [:user
+         {:dependencies [[incanter "1.5.7"]]
+          :injections [(use '[incanter core stats charts])]}]
+
+ :data [:user
+        {:dependencies [[cheshire "5.6.1"]
+                        [org.clojure/data.xml "0.0.8"]
+                        [clojure-csv/clojure-csv "2.0.1"]]
+         :injections [(use 'cheshire.core)
+                      (use 'clojure.data.xml)
+                      (use 'clojure-csv.core)]}]
+
+ :music [:user
+         {:dependencies [[overtone "0.10.1"]]
+          :injections [(use 'overtone.live)]}]
 }
