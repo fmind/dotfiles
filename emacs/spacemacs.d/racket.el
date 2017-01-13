a675756849951c292f2eefd2c66a1bc873e2008b
Freaxmind <freaxmind@users.noreply.github.com>
2017-01-13 09:19:10 +0100

    Defined more aliases

--- emacs/spacemacs.d/racket.el
+++ emacs/spacemacs.d/racket.el
@@ -1,13 +1,10 @@
                                         ; HOOKS
 
-;;(add-hook 'racket-mode-hook #'racket-unicode-input-method-enable)
-;;(add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
-(add-hood 'racket-describe-mode 'disable-evil-mode)
+(add-hook 'racket-describe-mode 'disable-evil-mode)
 
                                         ; KEYBINDINGS
 
 (spacemacs/set-leader-keys-for-major-mode 'racket-mode
   "," 'racket-run
   "m" 'racket-run-and-switch-to-repl
-  "d" 'spacemacs/jump-to-definition
-)
+  "d" 'spacemacs/jump-to-definition)
