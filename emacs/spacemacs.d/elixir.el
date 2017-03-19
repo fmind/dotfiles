d803c0f1780c2fc2fd78024a822cbd9740140bd8
fmind <fmind@users.noreply.github.com>
2017-03-19 19:22:38 +0100

    Added elixir shortcut and dev

--- emacs/spacemacs.d/elixir.el
+++ emacs/spacemacs.d/elixir.el
@@ -1,12 +1,39 @@
+                                        ; FUNCTIONS
+
+(defun my-elixir-switch-to-repl ()
+  "Switch to elixir repl."
+  (interactive)
+  (switch-to-buffer "*Alchemist-IEx*"))
+
+(defun my-elixir-switch-to-report ()
+  "Switch to elixir test report."
+  (interactive)
+  (switch-to-buffer "*alchemist test report*"))
+
+(defun my-elixir-open-mix-file ()
+  "Open the mix.exs file of the project."
+  (interactive)
+  (find-file (expand-file-name "mix.exs" (projectile-project-root))))
+
                                         ; BINDINGS
 
 (spacemacs/set-leader-keys-for-major-mode 'elixir-mode
+  ;; project
+  "pm" 'my-elixir-open-mix-file
+  ;; tests
+  "to" 'alchemist-test-toggle-test-report-display
+  ;; help
   "hh" 'alchemist-help
   "hi" 'alchemist-info-datatype-at-point
-  "to" 'alchemist-test-toggle-test-report-display
+  ;; goto
+  "`" 'my-elixir-switch-to-repl
+  "~" 'my-elixir-switch-to-report
+  ;; mix
+  "M" 'alchemist-mix
+  ;; iex
   "\"" 'alchemist-iex-project-run
   "b" 'alchemist-iex-reload-module
-  "," 'alchemist-iex-compile-this-buffer
+  "k" 'alchemist-iex-compile-this-buffer
   "l" 'alchemist-iex-send-current-line
   "L" 'alchemist-iex-send-current-line-and-go
   "r" 'alchemist-iex-send-region
