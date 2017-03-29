55d0b44890518e4715d1cb6ef40c42738bba59c4
fmind <fmind@users.noreply.github.com>
2017-03-29 17:48:42 +0200

    Refactored into smaller items

--- boot.sh
+++ boot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "[*] Installing git, python3, ansible"
-sudo apt-get install -y git python3 ansible
+echo "[*] Installing git, ansible, python3"
+sudo apt-get install -y git ansible python3
 
 echo
-read -p "Do you want to clone dotfiles [Y/y] ? " -n 1 -r
+read -p "Do you want to clone fmind/dotfiles [Y/y] ? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "[*] Cloning fmind/dotfiles"
@@ -12,7 +12,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 fi
 
 echo
-read -p "Do you want to install pip3 (user) [Y/y] ? " -n 1 -r
+read -p "Do you want to install pip3 (for current user) [Y/y] ? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
