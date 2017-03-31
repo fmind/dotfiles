6fd9c5410b19c6f5fab02832cca60dae1f87b485
fmind <fmind@users.noreply.github.com>
2017-03-31 17:46:37 +0200

    Added support to ideavimrc

--- languages/clojure/profiles.clj
+++ languages/clojure/profiles.clj
@@ -1,31 +1,30 @@
 {
- :user {:plugins [[lein-try "0.4.3"]
-                  [lein-exec "0.3.6"]
+ :user {:plugins [[lein-codox "0.10.3"]
                   [lein-cljfmt "0.5.6"]
                   [lein-ancient "0.6.10"]
-                  ;;[venantius/ultra "0.5.1"]
-                  [io.aviso/pretty "0.1.26"]
+                  [venantius/ultra "0.5.1"]
                   [jonase/eastwood "0.2.3"]]
-        :dependencies [[alembic "0.3.2"]
-                       [spyscope "0.1.6"]
-                       [io.aviso/pretty "0.1.26"]
-                       [im.chit/vinyasa "0.4.7"]
-                       [org.clojure/tools.nrepl "0.2.12"]
-                       [org.clojure/tools.namespace "0.2.10"]
-                       [leiningen #=(leiningen.core.main/leiningen-version)]]
-        :injections [(require 'clojure.repl)
-                     (require 'spyscope.core)
-                     (require '[vinyasa.inject :as inject])
-                     (inject/in
-                      [alembic.still :refer [distill]]
-                      [clojure.pprint :refer [pprint]]
-                      [clojure.java.shell :refer [sh]]
-                      [clojure.tools.namespace.repl :refer [refresh]]
-                      [clojure.repl :refer [apropos dir doc source find-doc]]
-                      ;;[vinyasa.lein :exclude [*project*]]
-                      ;;[vinyasa.inject :refer [inject [in inject-in]]]
-                      [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
-                      )]}
+        ; :dependencies [[alembic "0.3.2"]
+        ;                [spyscope "0.1.6"]
+        ;                [io.aviso/pretty "0.1.26"]
+        ;                [im.chit/vinyasa "0.4.7"]
+        ;                [org.clojure/tools.nrepl "0.2.12"]
+        ;                [org.clojure/tools.namespace "0.2.10"]
+        ;                [leiningen #=(leiningen.core.main/leiningen-version)]]
+        ; :injections [(require 'clojure.repl)
+        ;              (require 'spyscope.core)
+        ;              (require '[vinyasa.inject :as inject])
+        ;              (inject/in
+        ;               [alembic.still :refer [distill]]
+        ;               [clojure.pprint :refer [pprint]]
+        ;               [clojure.java.shell :refer [sh]]
+        ;               [clojure.tools.namespace.repl :refer [refresh]]
+        ;               [clojure.repl :refer [apropos dir doc source find-doc]]
+        ;               ;;[vinyasa.lein :exclude [*project*]]
+        ;               ;;[vinyasa.inject :refer [inject [in inject-in]]]
+        ;               ; [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
+                      ; )]}
+                      }
  :sh [:user
          {:dependencies [[me.raynes/fs "1.4.6"]]
           :injections[(use '[clojure.java.shell])
@@ -38,7 +37,7 @@
                       (import java.net.URL)]}]
  :selenium [:user
             {:dependencies [[clj-webdriver/clj-webdriver "0.7.2"]
-                           [org.seleniumhq.selenium/selenium-java "3.0.1"]]
+                           [org.seleniumhq.selenium/selenium-java "3.3.1"]]
             :injections [(use 'clj-webdriver.taxi)
                          (set-driver! {:browser :firefox})]}]
  :maths [:user
@@ -58,7 +57,7 @@
                       (require '[clojure.data.xml :as xml])
                       (require '[clojure-csv.core :as csv])]}]
  :graph [:user
-         {:dependencies [[aysylu/loom "0.6.0"]]
+         {:dependencies [[aysylu/loom "1.0.0"]]
           :injections [(require '[loom
                                    [graph :as Graph]
                                    [attr :as attr]
