e0ac32823160e1a5389b8ad8406faa86d59611f7
fmind <fmind@users.noreply.github.com>
2017-05-31 13:25:05 +0200

    Added haskell bindings

--- editors/emacs/spacemacs.d/haskell.el
+++ editors/emacs/spacemacs.d/haskell.el
@@ -1,10 +1,22 @@
                                         ; KEYBINDINGS
 
 (spacemacs/set-leader-keys-for-major-mode 'haskell-mode
-  "'" 'haskell-interactive-bring
-  "`" 'haskell-interactive-switch
-  "," 'haskell-process-load-file
-  "C" 'haskell-interactive-mode-clear)
+  "'" 'intero-repl
+  "," 'intero-repl-load
+  "\"" 'intero-repl-restart
+  "e" 'intero-repl-eval-region
+  "k" 'intero-repl-clear-buffer
+  "a" 'intero-apply-suggestions
+  "f" 'intero-info
+  "y" 'intero-type-at
+  "u" 'intero-uses-at
+  "j" 'intero-goto-definition
+  "l" 'intero-expand-splice-at-point
+  "C" 'intero-cd
+  "R" 'intero-restart
+  "P" 'intero-targets
+  "B" 'intero-list-buffers
+  )
 
 (spacemacs/set-leader-keys-for-major-mode 'haskell-interactive-mode
   "`" 'haskell-interactive-switch-back)
