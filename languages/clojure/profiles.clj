3b7b21a449b7bce8bd16df11f7a7d02db23c3d56
fmind <fmind@users.noreply.github.com>
2017-08-30 09:37:37 +0200

    Updated profiles.clj

--- languages/clojure/profiles.clj
+++ languages/clojure/profiles.clj
@@ -1,9 +1,9 @@
 {
  :user     {:plugins [
-                      ;;[lein-codox "0.10.3"]
-                      ;;[lein-cljfmt "0.5.6"]
+                      [lein-codox "0.10.3"]
+                      [lein-cljfmt "0.5.7"]
                       [lein-ancient "0.6.10"]
-                      ;; [venantius/ultra "0.5.1"]
+                      ;;[venantius/ultra "0.5.1"]
                       ;;[jonase/eastwood "0.2.3"]
                       ]
             :dependencies [
@@ -26,8 +26,13 @@
                            [clojure.repl :refer [apropos dir doc source find-doc]]
                            ;;[vinyasa.lein :exclude [*project*]]
                            ;;[vinyasa.inject :refer [inject [in inject-in]]]
-                           ; [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
+                           ;;[vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
                            )]}
+ :nb       [:user
+            {:dependencies [[lein-gorilla "0.4.0"]]
+             :injections   [(require '[gorilla-repl.core :refer [run-gorilla-server]])
+                            (run-gorilla-server {:port 8990})
+                            (./sh "xdg-open" "http://127.0.0.1:8990/worksheet.html")]}]
  :sh       [:user
             {:dependencies [[me.raynes/fs "1.4.6"]]
              :injections   [(use '[clojure.java.shell])
@@ -40,7 +45,7 @@
                             (import java.net.URL)]}]
  :selenium [:user
             {:dependencies [[clj-webdriver/clj-webdriver "0.7.2"]
-                            [org.seleniumhq.selenium/selenium-java "3.4.0"]]
+                            [org.seleniumhq.selenium/selenium-java "3.5.3"]]
              :injections   [(use 'clj-webdriver.taxi)
                             (set-driver! {:browser :firefox})]}]
  :maths    [:user
@@ -53,7 +58,7 @@
              :injections   [(use '[incanter core stats charts])]}]
 
  :data     [:user
-            {:dependencies [[cheshire "5.7.1"]
+            {:dependencies [[cheshire "5.8.0"]
                             [org.clojure/data.xml "0.0.8"]
                             [clojure-csv/clojure-csv "2.0.2"]]
              :injections   [(require '[cheshire.core :as Json])
@@ -67,11 +72,6 @@
                                        [alg :as Alg]
                                        [gen :as Gen]
                                        [io :as Io]])]}]
- :nb       [:user
-            {:dependencies [[lein-gorilla "0.4.0"]]
-             :injections   [(require '[gorilla-repl.core :refer [run-gorilla-server]])
-                            (run-gorilla-server {:port 8990})
-                            (./sh "xdg-open" "http://127.0.0.1:8990/worksheet.html")]}]
  :draw     [:user
             {:dependencies [[quil "2.6.0"]]
              :injections   [(require '[quil.core :as Draw])]}]
