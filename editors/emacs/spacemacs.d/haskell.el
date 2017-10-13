c43451e42143b7d00fcc887886e27c4540441efd
fmind <fmind@users.noreply.github.com>
2017-10-13 21:47:30 +0200

    Added haskell

--- editors/emacs/spacemacs.d/haskell.el
+++ editors/emacs/spacemacs.d/haskell.el
@@ -1,22 +1,22 @@
                                         ; KEYBINDINGS
 
-(spacemacs/set-leader-keys-for-major-mode 'haskell-mode
-  "'" 'intero-repl
-  "," 'intero-repl-load
-  "\"" 'intero-repl-restart
-  "e" 'intero-repl-eval-region
-  "k" 'intero-repl-clear-buffer
-  "a" 'intero-apply-suggestions
-  "f" 'intero-info
-  "y" 'intero-type-at
-  "u" 'intero-uses-at
-  "j" 'intero-goto-definition
-  "l" 'intero-expand-splice-at-point
-  "C" 'intero-cd
-  "R" 'intero-restart
-  "P" 'intero-targets
-  "B" 'intero-list-buffers
-  )
+;; (spacemacs/set-leader-keys-for-major-mode 'haskell-mode
+;;   "'" 'intero-repl
+;;   "," 'intero-repl-load
+;;   "\"" 'intero-repl-restart
+;;   "e" 'intero-repl-eval-region
+;;   "k" 'intero-repl-clear-buffer
+;;   "a" 'intero-apply-suggestions
+;;   "f" 'intero-info
+;;   "y" 'intero-type-at
+;;   "u" 'intero-uses-at
+;;   "j" 'intero-goto-definition
+;;   "l" 'intero-expand-splice-at-point
+;;   "C" 'intero-cd
+;;   "R" 'intero-restart
+;;   "P" 'intero-targets
+;;   "B" 'intero-list-buffers
+;;   )
 
-(spacemacs/set-leader-keys-for-major-mode 'haskell-interactive-mode
-  "`" 'haskell-interactive-switch-back)
+;; (spacemacs/set-leader-keys-for-major-mode 'haskell-interactive-mode
+;;   "`" 'haskell-interactive-switch-back)
