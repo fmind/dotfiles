83acd6d9e212da84e4552c5b5650900e211b8f3c
fmind <fmind@users.noreply.github.com>
2017-10-15 16:58:11 +0200

    Fix deprecated

--- editors/emacs/spacemacs.d/haskell.el
+++ editors/emacs/spacemacs.d/haskell.el
@@ -1,22 +1,64 @@
                                         ; KEYBINDINGS
 
-;; (spacemacs/set-leader-keys-for-major-mode 'haskell-mode
-;;   "'" 'intero-repl
-;;   "," 'intero-repl-load
-;;   "\"" 'intero-repl-restart
-;;   "e" 'intero-repl-eval-region
-;;   "k" 'intero-repl-clear-buffer
-;;   "a" 'intero-apply-suggestions
-;;   "f" 'intero-info
-;;   "y" 'intero-type-at
-;;   "u" 'intero-uses-at
-;;   "j" 'intero-goto-definition
-;;   "l" 'intero-expand-splice-at-point
-;;   "C" 'intero-cd
-;;   "R" 'intero-restart
-;;   "P" 'intero-targets
-;;   "B" 'intero-list-buffers
-;;   )
+(spacemacs/set-leader-keys-for-major-mode 'haskell-mode
+  "-" 'haskell-mode-jump-to-filename-in-string
+  "=" 'haskell-mode-stylish-buffer
+  "\"" 'haskell-intero/display-repl
+  "'" 'haskell-intero/pop-to-repl
+  "," 'intero-repl-load
+  "A" 'haskell-process-cabal
+  "B" 'haskell-process-cabal-build
+  "C" 'intero-repl-clear-buffer
+  ;; "D"
+  ;; "E"
+  ;; "F" RESERVED -- stylish
+  ;; "G"
+  ;; "H"
+  ;; "I"
+  ;; "J"
+  "K" 'intero-destroy
+  "L" 'intero-list-buffers
+  ;; "M"
+  ;; "N"
+  ;; "O"
+  ;; "P"
+  ;; "Q"
+  "R" 'intero-restart
+  ;; "S"
+  ;; "T"
+  ;; "U"
+  "V" 'haskell-cabal-visit-file
+  ;; "W"
+  ;; "X"
+  ;; "Y"
+  ;; "Z"
+  "a" 'intero-apply-suggestions
+  "b" 'hlint-refactor-refactor-buffer
+  ;; "c" RESERVED -- cabal
+  ;; "d" RESERVED -- debug
+  "e" 'intero-repl-eval-region
+  ;; "f" RESERVED -- hindent
+  ;; "g" RESERVED -- navigation
+  ;; "h" RESERVED -- documentation
+  ;; "i" RESERVED -- intero
+  "j" 'intero-goto-definition
+  "k" 'hoogle
+  "l" 'hlint-refactor-refactor-at-point
+  "m" 'intero-expand-splice-at-point
+  "n" 'haskell-navigate-imports
+  "o" 'intero-info
+  "p" 'helm-hoogle
+  "q" 'intero-targets
+  ;; "r" RESERVED -- refactor
+  ;; "s" RESERVED -- repl
+  "t" 'intero-type-at
+  "u" 'intero-uses-at
+  "v" 'haskell-intero/insert-type
+  "w" 'intero-cd
+  "x" 'haskell-compile
+  "y" 'hayoo
+  "z" 'intero-devel-reload
+  )
 
-;; (spacemacs/set-leader-keys-for-major-mode 'haskell-interactive-mode
-;;   "`" 'haskell-interactive-switch-back)
+(spacemacs/set-leader-keys-for-major-mode 'haskell-interactive-mode
+  "," 'haskell-interactive-switch-back)
