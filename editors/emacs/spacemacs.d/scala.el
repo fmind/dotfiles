93079b846f51e9da3d8dcbc5b69df4279704c0c1
fmind <fmind@users.noreply.github.com>
2017-10-30 09:12:10 +0100

    Removed scala

---
+++ editors/emacs/spacemacs.d/scala.el
@@ -0,0 +1,23 @@
+                                        ; CONFIGURATIONS
+
+(setq 'ensime-startup-notification nil)
+;; (setq-default flycheck-scalastylerc "/usr/local/etc/scalastyle_config.xml")
+
+                                        ; KEYBINDINGS
+
+;; (spacemacs/set-leader-keys-for-major-mode 'scala-mode
+;;   "`" 'ensime
+;;   "a"
+;;   "b"
+;;   "c"
+;;   "d"
+;;   "e"
+;;   "f"
+;;   "g"
+;;   "h"
+;;   "i"
+;;   "j"
+;;   "k"
+;;   "l"
+;;   "m"
+
