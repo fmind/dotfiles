ea00855dff23712ebaf5acb04e8ed6af9d772a3a
fmind <fmind@users.noreply.github.com>
2017-11-09 08:12:20 +0100

    Edited spacemacs

--- editors/emacs/spacemacs.d/org.el
+++ editors/emacs/spacemacs.d/org.el
@@ -1,5 +1,15 @@
                                         ; CONFIGURATION
 
+;; agenda
+(setq org-agenda-files (list "~/org/home.org"
+                             "~/org/work.org" 
+                             "~/org/idea.org"
+                             "~/org/tool.org"))
+
 ;; plant-uml
 (setq org-plantuml-jar-path (expand-file-name "~/bin/plantuml.jar"))
 (org-babel-do-load-languages 'org-babel-load-languages '((plantuml . t)))
+
+                                        ; HOOKS
+
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
