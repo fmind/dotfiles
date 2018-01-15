981422d301a341b08c6a3929a0a5780ba0ac5b73
fmind <fmind@users.noreply.github.com>
2018-01-15 09:09:35 +0100

    Simplified profiles.clj

--- languages/clojure/profiles.clj
+++ languages/clojure/profiles.clj
@@ -1,10 +1,9 @@
 {
   :repl {:global-vars {*print-length* 1000}}
 
-  :user {:dependencies [[alembic "0.3.2"]]
+  :user {:dependencies []
          :plugins [[lein-ancient "0.6.15"]]
-         :global-vars {*warn-on-reflection* true}
+         ; :global-vars {*warn-on-reflection* true}
          :injections [(require '[clojure.repl :refer :all])
-                      (require '[alembic.still :refer :all])
                       (require '[clojure.pprint :refer :all])]}
 }
