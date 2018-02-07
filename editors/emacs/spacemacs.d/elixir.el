f8ec7e212e41d3ef037a89bd20f573279c950884
fmind <fmind@users.noreply.github.com>
2018-02-07 20:32:18 +0100

    Improved elixir configuration

--- editors/emacs/spacemacs.d/elixir.el
+++ editors/emacs/spacemacs.d/elixir.el
@@ -1,9 +1,11 @@
-                                        ; CONFIGURATIONS
+                                        ; CONFS
 
+(setq alchemist-hooks-test-on-save t)
+(setq flycheck-elixir-credo-strict t)
 (setq alchemist-test-ask-about-save nil)
 (setq alchemist-hooks-compile-on-save t)
 
-                                        ; FUNCTIONS
+                                        ; FUNCS
 
 (defun my-elixir-switch-to-repl ()
   "Switch to elixir repl."
@@ -20,29 +22,29 @@
   (interactive)
   (find-file (expand-file-name "mix.exs" (projectile-project-root))))
 
-                                        ; BINDINGS
+                                        ; KEYS
 
 (spacemacs/set-leader-keys-for-major-mode 'elixir-mode
 ;;   ;; tests
 ;;   "to" 'alchemist-test-toggle-test-report-display
   ;; help
-  "?" 'alchemist-help
-  "H" 'alchemist-help-search-at-point
+  ;; "?" 'alchemist-help
+  ;; "H" 'alchemist-help-search-at-point
 ;;   "hi" 'alchemist-info-datatype-at-point
   ;; goto
-  "S" 'my-elixir-switch-to-repl
-  "T" 'my-elixir-switch-to-report
+  ;; "S" 'my-elixir-switch-to-repl
+  ;; "T" 'my-elixir-switch-to-report
   ;; mix
-  ";" 'alchemist-mix
-  "C" 'alchemist-mix-compile
-  "M" 'my-elixir-open-mix-file
+  ;; ";" 'alchemist-mix
+  ;; "C" 'alchemist-mix-compile
+  ;; "M" 'my-elixir-open-mix-file
   ;; iex
-  "r" 'alchemist-iex-send-region
-  "R" 'alchemist-iex-send-region-and-go
-  "l" 'alchemist-iex-send-current-line
-  "L" 'alchemist-iex-send-current-line-and-go
-  "," 'alchemist-iex-compile-this-buffer
-  "." 'alchemist-iex-compile-this-buffer-and-go
+  ;; "r" 'alchemist-iex-send-region
+  ;; "R" 'alchemist-iex-send-region-and-go
+  ;; "l" 'alchemist-iex-send-current-line
+  ;; "L" 'alchemist-iex-send-current-line-and-go
+  ;; "," 'alchemist-iex-compile-this-buffer
+  ;; "." 'alchemist-iex-compile-this-buffer-and-go
 ;;   "\"" 'alchemist-iex-project-run
 ;;   "b" 'alchemist-iex-reload-module
 ;;   "k" 'alchemist-iex-compile-this-buffer
