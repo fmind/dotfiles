b4184c308ee82ddc7b8fbed71ca910de8d90ce5c
fmind <fmind@users.noreply.github.com>
2018-02-12 09:57:20 +0100

    Added new plugins

--- languages/clojure/profiles.clj
+++ languages/clojure/profiles.clj
@@ -1,9 +1,10 @@
 {
   :repl {:global-vars {*print-length* 1000}}
 
-  :user {:dependencies []
+  :user {:dependencies [[spyscope "0.1.6"]]
          :plugins [[lein-ancient "0.6.15"]]
          ; :global-vars {*warn-on-reflection* true}
-         :injections [(require '[clojure.repl :refer :all])
+         :injections [(require 'spyscope.core)
+                      (require '[clojure.repl :refer :all])
                       (require '[clojure.pprint :refer :all])]}
 }
