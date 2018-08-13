2ea2fc33b48a9478d943a95f529b0ba1c554466c
fmind <dev@fmind.me>
2018-08-13 10:13:40 +0200

    added bool switch

--- vim/vimrc.py
+++ vim/vimrc.py
@@ -1,16 +1,11 @@
 import snake
-import re
 
 
-@snake.key_map("<leader>H")
-def toggle_snake_case_camel_case():
+@snake.key_map("<leader>zb")
+def toggle_boolean():
     word = snake.get_word()
 
-    if "_" in word:
-        chunks = word.split("_")
-        camel_case = chunks[0] + "".join([chunk.capitalize() for chunk in chunks[1:]])
-        snake.replace_word(camel_case)
-    else:
-        chunks = filter(None, re.split("([A-Z][^A-Z]*)", word))
-        snake_case = "_".join([chunk.lower() for chunk in chunks])
-        snake.replace_word(snake_case)
+    if word == "True":
+        snake.replace_word("False")
+    elif word == "False":
+        snake.replace_word("True")
