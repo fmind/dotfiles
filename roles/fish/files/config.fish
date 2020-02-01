ff6cc238587369c99df8289a770fca103f58b3df
Médéric Hurier <fmind@fmind.me>
2020-02-01 10:34:59 +0100

    added fish back

---
+++ roles/fish/files/config.fish
@@ -0,0 +1,35 @@
+# CONFIGS
+set -g fish_greeting ''
+# EXPORTS
+set -gx EDITOR vim
+set -gx LANG en_US.UTF-8
+set -gx PATH venv/bin $HOME/bin $HOME/.local/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin
+# PROMPTS
+function fish_prompt
+    set -l last_status $status
+
+    set_color brred
+    printf "%s" (whoami)
+    set_color normal
+    printf " at "
+    set_color brgreen
+    printf "%s" (hostname)
+    set_color normal
+    printf " in "
+    set_color brblue
+    printf "%s" (prompt_pwd)
+    set_color brmagenta
+    printf "%s" (__fish_git_prompt)
+    set_color normal
+
+    if test $last_status -ne 0
+        printf " ! "
+    else if test (id -u) -eq 0
+        printf " # "
+    else
+        printf " \$ "
+    end
+end
+# PLUGINS
+eval $HOME/.anaconda/bin/conda "shell.fish" "hook" $argv | source
+status --is-login; and status --is-interactive; and exec byobu-launcher
