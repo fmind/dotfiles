3ba141818a10ddcc736a47c8937e8697200617f4
Médéric Hurier <fmind@fmind.me>
2021-06-14 09:01:09 +0200

    Switched to spacefish prompt with fish.

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -2,33 +2,19 @@
 source $HOME/.aliases
 # CONFIGS
 set -g fish_greeting ''
+# KEYBIND
+function fish_hybrid_key_bindings
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings fish_hybrid_key_bindings
 # EXPORTS
 set -gx EDITOR vim
 set -gx LANG en_US.UTF-8
-set -gx PATH $HOME/bin venv/bin $HOME/.local/bin /snap/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin
-# PROMPTS
-function fish_prompt
-    set -l last_status $status
-
-    set_color brred
-    printf "%s" (whoami)
-    set_color normal
-    printf " at "
-    set_color brgreen
-    printf "%s" (hostname)
-    set_color normal
-    printf " in "
-    set_color brblue
-    printf "%s" (prompt_pwd)
-    set_color brmagenta
-    printf "%s" (__fish_git_prompt)
-    set_color normal
-
-    if test $last_status -ne 0
-        printf " ! "
-    else if test (id -u) -eq 0
-        printf " # "
-    else
-        printf " \$ "
-    end
-end
+set -gx PYTHONBREAKPOINT ipdb.set_trace
+set -gx PATH venv/bin $HOME/.local/bin /snap/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin
+# EXTENDS
+direnv hook fish | source
+starship init fish | source
