56b758bdcd82525a3f300a41a3e9755a52c0d585
Médéric Hurier (Fmind) <github@fmind.dev>
2026-04-10 11:26:23 +0200

    Set custom FZF_DEFAULT_OPTS for improved appearance
    Add custom color options for FZF.

--- dot_config/fish/conf.d/env.fish
+++ dot_config/fish/conf.d/env.fish
@@ -14,3 +14,9 @@ set -gx FZF_ALT_C_OPTS "--preview 'eza --tree --color=always {} | head -200'"
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set -gx FZF_CTRL_T_OPTS "--preview 'bat -n --color=always {}'"
 set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git"
+set -gx FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+--color=selected-bg:#45475A \
+--color=border:#6C7086,label:#CDD6F4"
