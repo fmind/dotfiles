0ba7bc1ce222539f3de9abfd974a3d65c2c6a4fa
Médéric Hurier (Fmind) <github@fmind.dev>
2026-04-12 21:36:51 +0200

    feat: add Angular and Flutter support, update Neovim plugins, and configure Zellij layouts

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,1 +1,13 @@
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "kdl", "just", "fish", "bash" })
+        opts.ensure_installed = vim.tbl_filter(function(lang)
+          return lang ~= "gitcommit"
+        end, opts.ensure_installed)
+      end
+    end,
+  },
+}
