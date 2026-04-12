a4423c354d9beb9b9b56a7ed9eb86246ae3f84e6
Médéric Hurier (Fmind) <github@fmind.dev>
2026-04-12 17:30:14 +0200

    refactor: update mise tool locks, reconfigure copilot and zellij, and clean up fish shell configurations

--- dot_config/fish/conf.d/env.fish
+++ dot_config/fish/conf.d/env.fish
@@ -1,13 +1,12 @@
-# Core system setup
+# Core setup
 set -gx EDITOR nvim
-set -gx LANG en_US.UTF-8
-set -gx MANPAGER "nvim +Man!"
-set -gx PAGER bat
 set -gx VISUAL nvim
+set -gx LANG en_US.UTF-8
+set -gx PAGER "less -FRX"
+set -gx MANPAGER "nvim +Man!"
 
-# Ripgrep config path
-set -gx RIPGREP_CONFIG_PATH "$HOME/.ripgreprc"
+# Ripgrep config
+set -gx RIPGREP_CONFIG_PATH ~/.ripgreprc
 
-# Catppuccin Mocha colors for tools
-set -gx GLAMOUR_STYLE catppuccin-mocha
-set -gx JQ_COLORS "1;30:0;39:0;39:0;39:0;32:1;39:1;39"
+# GitHub Copilot setup
+set -gx COPILOT_ALLOW_ALL true
