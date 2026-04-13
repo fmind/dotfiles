ec0099f887e3db9f7d7f0ad6d075c61ca9070210
Médéric Hurier (Fmind) <github@fmind.dev>
2026-04-13 19:13:44 +0200

    Configure markdown soft wrap

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -5,3 +5,14 @@ local node_host = vim.fn.exepath("neovim-node-host")
 if node_host ~= "" then
 	vim.g.node_host_prog = node_host
 end
+
+-- Markdown: soft wrap, no hard wrap
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.wrap = true -- enable soft wrap
+		vim.opt_local.linebreak = true -- break lines at words
+		vim.opt_local.textwidth = 0 -- disable hard wrap
+		vim.opt_local.formatoptions:remove("t") -- disable auto-wrap
+	end,
+})
