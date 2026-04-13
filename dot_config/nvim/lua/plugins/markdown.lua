b77d14f69c965d42b351e1a348384e0ce32be7a1
Médéric Hurier (Fmind) <github@fmind.dev>
2026-04-13 19:13:44 +0200

    Configure markdown soft wrap

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -6,7 +6,7 @@ return {
         prettier = {
           prepend_args = function(_, ctx)
             if vim.bo[ctx.buf].filetype == "markdown" then
-              return { "--prose-wrap", "always", "--print-width", "80" }
+              return { "--prose-wrap", "never" }
             end
             return {}
           end,
