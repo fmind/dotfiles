3721bf3e61a5d3d263d16fa91315038fe3be2c56
Médéric Hurier (Fmind) <github@fmind.dev>
2026-08-01 18:47:48 +0200

    feat(github): add agent-runnable issue queue
    Closes #42.

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -14,6 +14,8 @@ if status is-interactive
     abbr -a f fd
     # g:git
     abbr -a g git
+    # gd:gh-dash
+    abbr -a gd gh-dash
     # go:go
     abbr -a gob "go build"
     abbr -a gom "go mod tidy"
